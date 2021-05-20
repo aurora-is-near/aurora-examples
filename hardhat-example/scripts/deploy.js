8009e8a21dfaba39312bceada6cfb1e054d63a5b
Septen <gammerxpower@gmail.com>
2021-05-20 05:13:02 +0300

    Hardhat: fix deploy example.

--- hardhat-example/scripts/deploy.js
+++ hardhat-example/scripts/deploy.js
@@ -3,40 +3,45 @@
 //
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+require('dotenv').config();
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile 
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+    // Hardhat always runs the compile task when running scripts with its command
+    // line interface.
+    //
+    // If this script is run directly using `node` you may want to call compile
+    // manually to make sure everything is compiled
+    // await hre.run('compile');
 
-  // We get the contract to deploy
-  const [deployer] = await hre.ethers.getSigners();
+    const provider = hre.ethers.provider;
+    const deployerWallet = new hre.ethers.Wallet(process.env.AURORA_PRIVATE_KEY, provider);
 
-  console.log(
-    "Deploying contracts with the account:",
-    deployer.address
-  );
-  
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+    console.log(
+        "Deploying contracts with the account:",
+        deployerWallet.address
+    );
 
+    console.log(
+        "Account balance:",
+        (await deployerWallet.getBalance()).toString()
+    );
 
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+    const Greeter = await hre.ethers.getContractFactory("Greeter");
+    const options = { gasLimit: 1000000 };
+    const greeter = await Greeter
+        .connect(deployerWallet)
+        .deploy("Hello, Hardhat!", options);
+    await greeter.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+    console.log("Greeter deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
