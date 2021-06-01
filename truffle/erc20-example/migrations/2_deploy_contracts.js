764e69d8d870bfb1db6be44cf330a778ecf7d725
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-06-01 16:12:43 +0200

    mv erc20 to hardhat examples

--- truffle/erc20-example/migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-var WatermelonToken = artifacts.require("WatermelonToken");
-const BN = require('bignumber.js')
-
-module.exports = function(deployer) {
-    deployer.deploy(WatermelonToken, BN(1000000000000000000000));
-};
