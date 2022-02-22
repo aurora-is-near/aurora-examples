056ce7ef9917fef9bb4d7d5a43f46d1a191bd6a2
zakerikk <spry.note9499@fastmail.com>
2022-02-22 16:42:11 +0000

    Fix truffle erc721 deploy

--- truffle/erc721-example/truffle-config.js
+++ truffle/erc721-example/truffle-config.js
@@ -1,26 +1,10 @@
-const HDWalletProvider = require('truffle-hdwallet-provider')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
 const NonceTrackerSubprovider = require('web3-provider-engine/subproviders/nonce-tracker')
 const utils = require('web3-utils')
-const MNEMONIC = process.env.MNEMONIC || process.env.MNEMONIC
+const MNEMONIC = process.env.MNEMONIC
 const startIndex = 0
-const numberOfAccounts = 3
+const numberOfAddresses = 3
 let hdWalletProvider
-
-const setupWallet = (
-    url
-) => {
-    if (!hdWalletProvider) {
-        hdWalletProvider = new HDWalletProvider(
-            MNEMONIC,
-            url,
-            startIndex,
-            numberOfAccounts,
-	    true,
-	)
-        hdWalletProvider.engine.addProvider(new NonceTrackerSubprovider())
-    }
-    return hdWalletProvider
-}
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -31,11 +15,20 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
-    aurora: {
-      provider: () => setupWallet('https://testnet.aurora.dev'),
-      network_id: 0x4e454153,
+    aurora_testnet: {
+      provider: function() {
+        return new HDWalletProvider({
+          mnemonic: MNEMONIC,
+          providerOrUrl: 'https://testnet.aurora.dev',
+          numberOfAddresses
+        });
+      },
+      network_id: '1313161555',
       gas: 10000000,
-      from: '0x6A33382de9f73B846878a57500d055B981229ac4'
+      from: '0x23a824dd17d6571e1badd25a6247c685d6802985',
+      deploymentPollingInterval: 8000,
+      timeoutBlocks: 500,
+      confirmations: 10,
     },
     ropsten: {
       provider: () => setupWallet(`https://ropsten.infura.io/v3/${process.env.INFURA_TOKEN}`),
